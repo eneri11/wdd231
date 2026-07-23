18a2603e01f728afad600e7dce0cbe82b898f858
Irene Gavina Costales <>
2026-07-23 19:21:08 +0800

    commit

--- modal.js
+++ modal.js
@@ -1,7 +1,11 @@
-const modal = document.querySelector('#myModal');
-const closeModal = document.querySelector('#closeModal');
-modal.showModal(); // display the modal dialog right away.
-// Usually you will want to wait for a user action to show the modal dialog
-closeModal.addEventListener('click', () => {
-  modal.close();
+const modal = document.querySelector("#infoModal");
+const openBtn = document.querySelector("#openModal");
+const closeBtn = document.querySelector("#closeModal");
+
+openBtn.addEventListener("click", () => {
+  modal.showModal(); // Opens modal + backdrop
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.close(); // Closes modal
 });
