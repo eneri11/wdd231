2dfc765049e1eb24f6830daa3bd3c4f63a8e2f92
Irene Gavina Costales <>
2026-07-27 02:33:34 +0800

    commit

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -13,3 +13,19 @@ document.querySelectorAll('.close-modal').forEach(button => {
     e.target.closest('dialog').close();
   });
 });
+
+const currentUrl = window.location.href;
+const formData = new URLSearchParams(window.location.search);
+
+const resultsElement = document.getElementById('results');
+
+if (formData) {
+  resultsElement.innerHTML = `
+    <p><strong>First Name:</strong> ${formData.get('fname')}</p>
+    <p><strong>Last Name:</strong> ${formData.get('lname')}</p>
+    <p><strong>Email:</strong> ${formData.get('email')}</p>
+    <p><strong>Mobile Phone:</strong> ${formData.get('phone')}</p>
+    <p><strong>Organization Name:</strong> ${formData.get('organization')}</p>
+    <p><strong>Submitted Date:</strong> ${formData.get('timestamp')}</p>
+  `;
+}
